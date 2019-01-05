fc0a919e68da13088b99afc40b9feff2c4329686
SimasDei <simondeiman@gmail.com>
2019-01-05 16:48:36 +0200

    Establishing front end

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import Routes from './Routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const storeAndMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(
+  createStore
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={storeAndMiddleware(reducers)}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
